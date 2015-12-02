09cf0a51dbb05cb921fdf95933ca1df118db67b0
ravitezu <ravi-teja@live.com>
2015-12-02 12:33:57 +0530

    more code

--- iptoloc.go
+++ iptoloc.go
@@ -34,10 +34,6 @@ import (
 	"os"
 )
 
-//{"as":"AS14907 Wikimedia Foundation Inc.","city":"San Francisco","country":"United States","countryCode":"US","isp":"Wikimedia Foundation",
-//"lat":37.7898,"lon":-122.3942,"org":"Wikimedia Foundation","query":"208.80.152.201","region":"CA","regionName":"California","status":"success",
-//"timezone":"America/Los_Angeles","zip":"94105"}
-
 //APIEndpoint - contains the API URL.
 const (
 	APIEndpoint = "http://ip-api.com/json/"
